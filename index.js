e95a0c95715f2dfb5ce09606975e00fabf1e68f0
Salvador Martinez <67123399+salvadormartin3z@users.noreply.github.com>
2021-11-23 00:14:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import Game from './juego.js';
+const TABLERO = document.getElementById('main');
+const X = 30;
+const Y = 30;
+const VIVAS = ['2,3', '2,4', '2,5'];
+const GAME = new Game(X, Y, TABLERO, VIVAS);
+GAME.crearTablero();
+
+setInterval(() => {
+  GAME.buscarCelulas();
+}, 100);
